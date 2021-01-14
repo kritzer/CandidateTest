7f7fb328ecf194b72b1194eef645b3e1fced0d9d
Kritzer <kissyoukissme93@gmail.com>
2021-01-14 19:11:56 +0700

    เริ่มอัพหน้าบ้าน

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './page/home/home.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/components/form-data/form-data.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormDataService } from 'src/app/services/form-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-form-data',
+  templateUrl: './form-data.component.html',
+  styleUrls: ['./form-data.component.css']
+})
+export class FormDataComponent implements OnInit {
+title = '';
+formdata: FormData;
+str: string;
+snackbar = true;
+  constructor(private fdts: FormDataService,
+              private snackBar: MatSnackBar) {
+
+  }
+  ngOnInit(): void {
+    this.formdata = new FormData();
+  }
+
+  onSubmit(form) {
+    this.str = 'ค่าจากฟอร์ม: ' + JSON.stringify(this.formdata);
+    this.fdts.addFormData(form).subscribe( data => {
+      console.log(this.str);    }
+      );
+  }
+
+}
+class FormData {
+  fname: string;
+  lname: string;
+  age: number;
+  job: string;
+}
